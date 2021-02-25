c563260620149e6e820b5f7c612c23a64979d87c
sj9608 <sneak7581@wku.ac.kr>
2021-02-25 09:56:52 +0900

    C01 is modified to be evaluated

--- C01/ex06/ft_strlen.c
+++ C01/ex06/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: seungjle <seungjle@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 21:19:08 by seungjle          #+#    #+#             */
-/*   Updated: 2021/02/23 21:22:06 by seungjle         ###   ########.fr       */
+/*   Updated: 2021/02/25 09:36:25 by seungjle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@ int	ft_strlen(char *str)
 {
 	int length;
 
-	length = 1;
+	length = 0;
+	if (str[0] == '\0')
+		return (0);
 	while (str[length] != '\0')
 	{
 		length++;
