e6570462c8fbf8dd5f527c95f0b770851b2981ac
sj9608 <sneak7581@wku.ac.kr>
2021-03-08 00:16:31 +0900

    strncpy modified

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: seungjle <seungjle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 14:59:31 by seungjle          #+#    #+#             */
-/*   Updated: 2021/03/07 17:12:18 by seungjle         ###   ########.fr       */
+/*   Updated: 2021/03/07 23:55:29 by seungjle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,27 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 		dest[i] = src[i];
 		i++;
 	}
-	while (dest[i] != '\0' && i < n)
+	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
+}
+
+#include <string.h>
+#include <stdio.h>
+
+int		main(void)
+{
+	char c[10] = "hello";
+	char c1[10] = "bdceaaa";
+	char c2[10] = "bdceaaa";
+
+	ft_strncpy(c1, c, 7);
+	strncpy(c2, c , 7);
+	printf("c1 : %s\n", c1);
+	printf("c2 : %s\n", c2);
+
+	return (0);
 }
