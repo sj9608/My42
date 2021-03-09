50a0ad9ec5e6c8608a666f5b8ba3bc064a542af2
sj9608 <sneak7581@wku.ac.kr>
2021-03-09 17:02:56 +0900

    space aligned

--- C02/ex03/ft_str_is_numeric.c
+++ C02/ex03/ft_str_is_numeric.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seungjle <seungjle@studnet.42seoul.>       +#+  +:+       +#+        */
+/*   By: seungjle <seungjle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 17:38:20 by seungjle          #+#    #+#             */
-/*   Updated: 2021/02/25 19:40:26 by seungjle         ###   ########.fr       */
+/*   Updated: 2021/03/09 17:02:49 by seungjle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_numeric(char *str)
+int		ft_str_is_numeric(char *str)
 {
 	int index;
 
