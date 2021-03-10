3df14573b027cd5cacdd51b49dbe2e4547a34eca
LSJ <sneak7581@wku.ac.kr>
2021-03-11 00:14:01 +0900

    2021. 03. 11 Thu 01:12 C05 Optimize

--- C05/ex03/ft_recursive_power.c
+++ C05/ex03/ft_recursive_power.c
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_recursive_power.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seungjle <seungjle@studnet.42seoul.>       +#+  +:+       +#+        */
+/*   By: seungjle <seungjle@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 18:42:15 by seungjle          #+#    #+#             */
-/*   Updated: 2021/03/09 23:28:16 by seungjle         ###   ########.fr       */
+/*   Updated: 2021/03/10 23:10:00 by seungjle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_recursive_power(int nb, int power)
 {
+	if (power < 0)
+		return (1);
 	if (power == 0)
 		return (1);
 	return (nb * ft_recursive_power(nb, --power));
--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -3,26 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sqrt.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seungjle <seungjle@studnet.42seoul.>       +#+  +:+       +#+        */
+/*   By: seungjle <seungjle@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 21:55:01 by seungjle          #+#    #+#             */
-/*   Updated: 2021/03/09 23:28:50 by seungjle         ###   ########.fr       */
+/*   Updated: 2021/03/11 00:08:46 by seungjle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_sqrt(int nb)
 {
-	int sqrt;
-	int i;
+	unsigned int sqrt;
+	unsigned int i;
 
 	i = 1;
-	sqrt = 0;
-	if (nb < 1)
+	if (nb < 0)
 		return (0);
-	while (sqrt <= nb)
+	sqrt = 0;
+	while (sqrt <= (unsigned int)nb)
 	{
 		sqrt = i * i;
-		if (sqrt == nb)
+		if (sqrt == (unsigned int)nb)
 			return (i);
 		i++;
 	}
