12ec9c1d31b1ea312c655b69f0d15f45cbea97d1
sj9608 <sneak7581@wku.ac.kr>
2021-03-11 20:07:59 +0900

    power modify

--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -6,7 +6,7 @@
 /*   By: seungjle <seungjle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 18:29:39 by seungjle          #+#    #+#             */
-/*   Updated: 2021/03/11 18:55:59 by seungjle         ###   ########.fr       */
+/*   Updated: 2021/03/11 20:06:28 by seungjle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int		ft_iterative_power(int nb, int power)
 	int result;
 
 	result = 1;
-	if (nb < 0 || power < 0)
+	if (power < 0)
 		return (0);
 	if (power == 0)
 		return (1);
--- C05/ex03/ft_recursive_power.c
+++ C05/ex03/ft_recursive_power.c
@@ -6,13 +6,13 @@
 /*   By: seungjle <seungjle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 18:42:15 by seungjle          #+#    #+#             */
-/*   Updated: 2021/03/11 18:12:10 by seungjle         ###   ########.fr       */
+/*   Updated: 2021/03/11 20:06:19 by seungjle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_recursive_power(int nb, int power)
 {
-	if (nb < 0 || power < 0)
+	if (power < 0)
 		return (0);
 	if (power == 0)
 		return (1);
