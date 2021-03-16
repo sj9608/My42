166a80a35c36ac6ea5b53dcdc4c00d352475e661
sj9608 <sneak7581@wku.ac.kr>
2021-03-16 19:06:29 +0900

    ft_boolean update

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -10,8 +10,8 @@ typedef int t_bool;
 
 #define EVEN(nbr) (nbr % 2 == 0)
 
-#define EVEN_MSG "I have an even number of arguments."
-#define ODD_MSG "I have an odd number of arguments."
+#define EVEN_MSG "I have an even number of arguments.\n"
+#define ODD_MSG "I have an odd number of arguments.\n"
 
 #define SUCCESS 0
 #endif
